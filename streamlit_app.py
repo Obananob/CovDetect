7b6fcacef0477627566d66f6fe6cb0899e6bca16
Obananob <97780446+Obananob@users.noreply.github.com>
2025-07-25 16:33:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import os
 # --- Load TFLite Model ---
 @st.cache_resource
 def load_model():
-    interpreter = tf.lite.Interpreter(model_path="model.tflite")
+    interpreter = tf.lite.Interpreter(model_path="covdetect_model_float16.tflite")
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
